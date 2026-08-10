7b908681c86eb9794a2bb0ec002c3688af62e208
SagderovKamalIT <sagderovkamal.it@gmail.com>
2026-08-10 16:11:59 +0300

    update: complete exercice

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -2,9 +2,18 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1}></Part>
-      <Part part={props.part2} exercise={props.exercises2}></Part>
-      <Part part={props.part3} exercise={props.exercises3}></Part>
+      <Part
+        part={props.parts[0].name}
+        exercises={props.parts[0].exercises}
+      ></Part>
+      <Part
+        part={props.parts[1].name}
+        exercises={props.parts[1].exercises}
+      ></Part>
+      <Part
+        part={props.parts[2].name}
+        exercises={props.parts[2].exercises}
+      ></Part>
     </>
   );
 };
